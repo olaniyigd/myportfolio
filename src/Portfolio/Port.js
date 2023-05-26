e97b7598655b3cb2ecb8f7ade3ecaa04b789b6cd
olaniyigd <gbadegesinpharuq@yahoo.com>
2023-05-26 11:52:42 +0100

    first commit

--- src/Portfolio/Port.js
+++ src/Portfolio/Port.js
@@ -2,6 +2,8 @@ import "./Port.css";
 import portfolio from "../Portfolioimage/portfolio.jpeg";
 import weather from "../Portfolioimage/weather.jpeg";
 import gifted from "../Portfolioimage/gifted.jpeg";
+import techcorp from "../Portfolioimage/techcorp.png";
+import busd from "../Portfolioimage/busd.png";
 import React, { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
@@ -31,6 +33,22 @@ export default function Port () {
              <a href="https://giftedbrainz.vercel.app/">
                     <img src={gifted} alt="weather" className="weather"/>
                     <p className="description">Gifted-Brainz created with reactjs.</p>
+                    <p className="description">click the image to have a view.</p>
+                </a>
+            </figure>
+            </div>
+            <div className="port-up">
+            <figure data-aos="fade-up" className="figure">
+                <a href="https://techcorp-one.vercel.app/">
+                    <img src={techcorp} alt="portfolio" className="portfolio"/>
+                    <p className="description">page created with reactjs.</p>
+                    <p className="description">click the image to have a view.</p>
+                </a>
+            </figure>
+            <figure data-aos="fade-left" className="figure">
+             <a href="https://olaniyigd.github.io/busdconversion/api.html">
+                    <img src={busd} alt="weather" className="weather"/>
+                    <p className="description">BUSD conversion App created with html, css, and javascript.</p>
                     <p className="description">click the image to have a view.</p>
                 </a>
             </figure>
